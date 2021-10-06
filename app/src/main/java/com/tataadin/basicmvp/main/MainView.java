332edcdb0b41e1d46485d87c71e75adafcc081a4
tataadin <tataa2237@gmail.com>
2021-10-06 19:27:34 +0700

    add new view interface

--- app/src/main/java/com/tataadin/basicmvp/main/MainView.java
+++ app/src/main/java/com/tataadin/basicmvp/main/MainView.java
@@ -3,5 +3,7 @@ package com.tataadin.basicmvp.main;
 import com.tataadin.basicmvp.base.View;
 
 public interface MainView extends View {
-
+    void showDataInDialog(int a);
+    void showError();
+    void showSuccess();
 }
