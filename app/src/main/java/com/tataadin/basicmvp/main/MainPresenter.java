bd8bcf0e0e783629005dde32f2419d1684a3cbd3
tataadin <tataa2237@gmail.com>
2021-10-06 19:27:51 +0700

    new presenter function

--- app/src/main/java/com/tataadin/basicmvp/main/MainPresenter.java
+++ app/src/main/java/com/tataadin/basicmvp/main/MainPresenter.java
@@ -1,10 +1,10 @@
 package com.tataadin.basicmvp.main;
 
 import com.tataadin.basicmvp.base.Presenter;
-import com.tataadin.basicmvp.main.MainView;
 
 public class MainPresenter implements Presenter<MainView> {
     private MainView mainView;
+    private int count = 0;
     @Override
     public void onAttach(MainView view) {
         mainView = view;
@@ -13,5 +13,14 @@ public class MainPresenter implements Presenter<MainView> {
     @Override
     public void onDetach() {
         mainView = null;
+    }
+
+    void showData(String textA, String textB){
+        if (textA.isEmpty() && textB.isEmpty()){
+            mainView.showError();
+        }else {
+            mainView.showSuccess();
+            mainView.showDataInDialog(count++);
+        }
     }
 }
